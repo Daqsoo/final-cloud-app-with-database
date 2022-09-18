cc49d6b4e4d0d6660a9be2a4309eda437d381ccf
Daqsoo <Mr.Spy710@gmail.com>
2022-09-18 15:04:16 -0400

    fixed error

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,21 +1,20 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner,Question,Choice
+from .models import Course, Lesson, Instructor, Learner
+from .models import Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
-
-class QuestionInline(admin.StackedInline):
-    model = Question
-    
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
-    extra = 4
+    extra = 3
 
+class QuestionAdmin(admin.ModelAdmin):
+    model = Question
+    inlines = [ChoiceInline]
 
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
-
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
@@ -28,12 +27,10 @@ class CourseAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
-class QuestionAdmin(admin.ModelAdmin):
-    inlines = [ChoiceInline]
+
 # <HINT> Register Question and Choice models here
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
-admin.site.register(Question,QuestionAdmin)
-admin.site.register(Choice)
